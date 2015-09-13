16ce5836a52c05ae85aeb98fac46d2f975c2b052
ChrisOwens <cowens85@gmail.com>
2015-09-13 14:28:04 -0400

    initial try at strides

--- ps3/ps3.py
+++ ps3/ps3.py
@@ -3,6 +3,8 @@
 import numpy as np
 import cv2
 
+from numpy.lib.stride_tricks import as_strided
+
 import os
 
 input_dir = "input"  # read images from os.path.join(input_dir, <filename>)
@@ -16,7 +18,7 @@ def get_patch(matrix, y_up_offset, y_down_offset, x_left_offset, x_right_offset,
 
 def disparity_ssd(L, R, window_size = 21):
     """Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
-    
+
     Params:
     L: Grayscale left image, in range [0.0, 1.0]
     R: Grayscale right image, same size as L
@@ -39,6 +41,11 @@ def disparity_ssd(L, R, window_size = 21):
     left_shift = False
     right_shift = False
     search_range = width / 3
+
+    r_shape = (R.shape[0]-(window_size-1), R.shape[1]-(window_size-1), window_size, window_size)
+    r_strides = (R.shape[1] * R.itemsize, R.itemsize, R.itemsize * R.shape[1], R.itemsize)
+    r_strips = as_strided(R, r_shape, r_strides)
+
     for y in range(height):
         """ Compute Y Offsets """
         y_up_offset = offset if y >= offset else y
@@ -50,44 +57,91 @@ def disparity_ssd(L, R, window_size = 21):
             x_left_offset = offset if x >= offset else x
             x_right_offset = offset if x + offset < width else width - x - 1
 
+            r_strip = r_strips[y]
+
             l_patch = get_patch(L, y_up_offset, y_down_offset, x_left_offset, x_right_offset, y, x)
+            l_strip = as_strided(l_patch, r_strip.shape, (0, l_patch.itemsize*window_size, l_patch.itemsize))
 
-            # print "x", x
-            min_ssd = np.infty
-            x_prime_start = x_left_offset
-            x_prime_end = width-x_right_offset
 
-            # If we've figured out which way we're shifting,
-            # Stop looking for matches as if it was shifted the other way
-            # This adds speed and may decrease mistakes with repeating patterns
-            if left_shift:
-                x_prime_end = min(x + offset,x_prime_end)
-            elif right_shift:
-                x_prime_start = max(x - offset, x_prime_start)
-
-            # Take a guess on how far off things can shift and restrict to that area
-            x_prime_start = max (x_prime_start,x - search_range)
-            x_prime_end = min (x_prime_end,x + search_range)
-
-            for x_prime in range(x_prime_start, x_prime_end):
-                # print "x'", x_prime
-                r_patch = get_patch(R, y_up_offset, y_down_offset, x_left_offset, x_right_offset, y, x_prime)
-                # print "sum:", np.sum(r_patch)
-                ssd = np.sum((l_patch - r_patch)**2)
-                if ssd < min_ssd:
-                    min_ssd = ssd
-                    D[y][x] = x_prime - x
-
-        # Try to figure out if we're shifting left or right.
-        # Assuming no motion in the image, there should only be one shift
-        if left_shift is False and right_shift is False:
-            sum = np.sum(D[y])
-            if sum < 0:
-                left_shift = True
-            elif sum > 0:
-                right_shift = True
+            ssd = ((l_strip - r_strip)**2).sum((1,2))
+            print "ssd\n",ssd
 
     return D
+
+# def disparity_ssd(L, R, window_size = 21):
+#     """Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
+#
+#     Params:
+#     L: Grayscale left image, in range [0.0, 1.0]
+#     R: Grayscale right image, same size as L
+#
+#     Returns: Disparity map, same size as L, R
+#     """
+#
+#     # kernel_size = 5
+#     # sigma = 3
+#     # cv2.GaussianBlur(L.copy(), (kernel_size,kernel_size), sigma)
+#     # cv2.GaussianBlur(R.copy(), (kernel_size,kernel_size), sigma)
+#
+#     D = np.zeros(L.shape, dtype=np.float)
+#
+#     # subtract 1 due to the starting pixel
+#     offset = (window_size - 1) / 2
+#     shape = L.shape
+#     height = shape[0]
+#     width = shape[1]
+#     left_shift = False
+#     right_shift = False
+#     search_range = width / 3
+#     for y in range(height):
+#         """ Compute Y Offsets """
+#         y_up_offset = offset if y >= offset else y
+#         y_down_offset = offset if y + offset < height else height - y - 1
+#         # print "y d off:", y_down_offset
+#
+#         for x in range(width):
+#             """ Compute X Offsets """
+#             x_left_offset = offset if x >= offset else x
+#             x_right_offset = offset if x + offset < width else width - x - 1
+#
+#             l_patch = get_patch(L, y_up_offset, y_down_offset, x_left_offset, x_right_offset, y, x)
+#
+#             # print "x", x
+#             min_ssd = np.infty
+#             x_prime_start = x_left_offset
+#             x_prime_end = width-x_right_offset
+#
+#             # If we've figured out which way we're shifting,
+#             # Stop looking for matches as if it was shifted the other way
+#             # This adds speed and may decrease mistakes with repeating patterns
+#             if left_shift:
+#                 x_prime_end = min(x + offset,x_prime_end)
+#             elif right_shift:
+#                 x_prime_start = max(x - offset, x_prime_start)
+#
+#             # Take a guess on how far off things can shift and restrict to that area
+#             x_prime_start = max (x_prime_start,x - search_range)
+#             x_prime_end = min (x_prime_end,x + search_range)
+#
+#             for x_prime in range(x_prime_start, x_prime_end):
+#                 # print "x'", x_prime
+#                 r_patch = get_patch(R, y_up_offset, y_down_offset, x_left_offset, x_right_offset, y, x_prime)
+#                 # print "sum:", np.sum(r_patch)
+#                 ssd = np.sum((l_patch - r_patch)**2)
+#                 if ssd < min_ssd:
+#                     min_ssd = ssd
+#                     D[y][x] = x_prime - x
+#
+#         # Try to figure out if we're shifting left or right.
+#         # Assuming no motion in the image, there should only be one shift
+#         if left_shift is False and right_shift is False:
+#             sum = np.sum(D[y])
+#             if sum < 0:
+#                 left_shift = True
+#             elif sum > 0:
+#                 right_shift = True
+#
+#     return D
 
 def disparity_ncorr(L, R, window_size=19):
     """Compute disparity map D(y, x) such that: L(y, x) = R(y, x + D(y, x))
@@ -208,9 +262,9 @@ def main():
 
     """Run code/call functions to solve problems."""
     # 1
-    """
+    # """
     apply_disparity_ssd("pair0-L.png", "pair0-R.png", "1", 11)
-    """
+    # """
     # apply_disparity_norm("pair0-L.png", "pair0-R.png", "test", 3)
 
     # out = internet_ssd(cv2.imread(os.path.join('input', "pair0-L.png"), 0) * (1 / 255.0), cv2.imread(os.path.join('input', "pair0-R.png"), 0) * (1 / 255.0))
@@ -233,9 +287,9 @@ def main():
 
     # 2
     # TODO: Apply disparity_ssd() to pair1-L.png and pair1-R.png (in both directions)
-    # """
+    """
     apply_disparity_ssd("pair1-L.png", "pair1-R.png", "2", 15)
-    # """
+    """
 
     # 3
     # TODO: Apply disparity_ssd() to noisy versions of pair1 images
