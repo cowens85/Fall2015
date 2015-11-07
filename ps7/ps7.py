2655cea47cbc13e4d5cf8c3b6cb261abaf542629
ChrisOwens <cowens85@gmail.com>
2015-11-07 17:06:55 -0500

    $@

--- ps7/ps7.py
+++ ps7/ps7.py
@@ -27,20 +27,28 @@ class ParticleFilter(object):
         """
         self.num_particles = kwargs.get('num_particles', 100)  # extract num_particles (default: 100)
         # TODO: Your code here - extract any additional keyword arguments you need and initialize state
-        self.sigma = kwargs.get('num_particles', 10)
+        self.sigma = kwargs.get('sigma', 10)
 
         self.template = template
 
         #particles - x,y pairs
         self.particles = []
 
+
         #weights - same indicies as the particles (e.g. weight[3] applies to particles[3])
-        self.weights = []
+        #init weights to be uniform
+        self.weights = np.ones(self.num_particles,dtype=np.float) / self.num_particles
+        # self.weights = []
+
+        buf = 10
+        x_range = np.arange(kwargs.get('x') - buf,kwargs.get('x') + kwargs.get('w') + buf).astype(np.int)
+        y_range = np.arange(kwargs.get('y') - buf,kwargs.get('y') + kwargs.get('h') + buf).astype(np.int)
+        if should_print: print 'xrange', x_range
+        if should_print: print 'yrange', y_range
         for i in range(0,self.num_particles):
             #select a random (x,y)
-            self.particles.append((np.random.choice(frame.shape[1], 1)[0], np.random.choice(frame.shape[0], 1)[0]))
-            #init weights to be uniform
-            self.weights.append(1.0/self.num_particles)
+            self.particles.append((np.random.choice(x_range, 1)[0], np.random.choice(y_range, 1)[0]))
+            # self.weights.append(1.0/self.num_particles)
 
 
     def process(self, frame):
@@ -51,13 +59,12 @@ class ParticleFilter(object):
             frame: color BGR uint8 image of current video frame, values in [0, 255]
         """
 
-        if self.should_print:
-            print self.particles
-            print self.num_particles
-            print self.weights
+        # print self.particles
+        # print self.num_particles
+        # print self.weights
 
         #sample particles based on weights
-        newParticles = np.random.choice(self.num_particles, self.num_particles, True, self.weights)
+        # newParticles = np.random.choice(self.num_particles, self.num_particles, True, self.weights)
 
         #for each particle,
         #get frame centered at that point
@@ -70,15 +77,25 @@ class ParticleFilter(object):
 
         amountAdded = 0
         for i in range(0, self.num_particles):
+            if should_print : print "particles", self.particles[i]
+            patch = get_patch(frame, self.particles[i], self.template.shape)
 
-            patch = get_patch(frame, self.particles[i])
-            MSE = calc_mse(self.template, patch)
-            self.weights[i] += MSE
-            amountAdded += MSE
-            noise1 = np.random.normal(0, self.sigma, 1)
-            noise2 = np.random.normal(0, self.sigma, 1)
-            self.particles[i][0] += noise1
-            self.particles[i][1] += noise2
+            if should_print :
+                print "template:", self.template.shape
+                print "frame:", frame.shape
+                print "patch:", patch.shape
+
+            if patch.shape == self.template.shape:
+                MSE = calc_mse(self.template, patch)
+                self.weights[i] += MSE
+                amountAdded += MSE
+                noise0 = np.random.normal(0, self.sigma, 1)
+                noise1 = np.random.normal(0, self.sigma, 1)
+
+                if should_print:print "noise0: ", noise0, "  noise1: ", noise1
+
+                self.particles[i] = (int(self.particles[i][0] + noise1), int(self.particles[i][1] + noise1))
+                # self.particles[i][1] = noise2
 
         self.weights /= amountAdded
 
@@ -92,17 +109,40 @@ class ParticleFilter(object):
             frame_out: copy of frame to overlay visualization on
         """
         # Note: This may not be called for all frames, so don't do any model updates here!
-        pass  # TODO: Your code here - draw particles, tracking window and a circle to indicate spread
+        # TODO: Your code here - draw particles, tracking window and a circle to indicate spread
+        u_weighted_mean = 0
+        v_weighted_mean = 0
+        for i in range(len(self.particles)):
+            u = self.particles[i][0]
+            v = self.particles[i][1]
+            cv2.circle(frame_out, (int(u), int(v)), 1, (0, 0, 255))
+            u_weighted_mean += u * self.weights[i]
+            v_weighted_mean += v * self.weights[i]
+
+        center = (int(u_weighted_mean), int(v_weighted_mean))
+        x, y, w, h = get_rect(center, self.template.shape)
+        cv2.rectangle(frame_out, (x, y), (x + w, y + h), (0, 255, 0))
+        cv2.circle(frame_out, center, 5, (0, 255, 0))
+
 
 def get_patch(frame, particle, shape_needed):
-    #upper left point (ish)
-    upperLeft = particle - np.array(shape_needed)/2
-    y = upperLeft[1]
-    x = upperLeft[0]
-    h = shape_needed[1]
-    w = shape_needed[0]
+    x, y, w, h = get_rect(particle, shape_needed)
 
-    frame[y:y + h, x:x + w]
+    if should_print:
+        print "y,h,x,w", y, h, x, w
+
+    return frame[y:y + h, x:x + w]
+
+
+def get_rect(point, shape_needed):
+    #upper left point (ish)
+    upperLeft = point - np.array(shape_needed)/2
+    x = int(upperLeft[0])
+    y = int(upperLeft[1])
+    h = int(shape_needed[0])
+    w = int(shape_needed[1])
+
+    return x, y, w, h
 
 def calc_mse(template, patch):
     return ((template - patch) ** 2).mean(axis=None)
@@ -157,7 +197,6 @@ def run_particle_filter(pf_class, video_filename, template_rect, save_frames={},
         save_frames: dictionary of frames to save {<frame number>|'template': <filename>}
         kwargs: arbitrary keyword arguments passed on to particle filter class
     """
-
     # Open video file
     video = cv2.VideoCapture(video_filename)
 
@@ -166,20 +205,40 @@ def run_particle_filter(pf_class, video_filename, template_rect, save_frames={},
     pf = None
     frame_num = 0
 
-    # Loop over video (till last frame or Ctrl+C is presssed)
+    # Loop over video (till last frame or Ctrl+C is pressed)
     while True:
         try:
             # Try to read a frame
             okay, frame = video.read()
             if not okay:
+                print "done"
                 break  # no more frames, or can't read video
+
+            color_frame = frame.copy()
+            frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY).astype(np.float)
 
             # Extract template and initialize (one-time only)
             if template is None:
-                template = frame[int(template_rect['y']):int(template_rect['y'] + template_rect['h']),
-                                 int(template_rect['x']):int(template_rect['x'] + template_rect['w'])]
+                y = int(template_rect['y'])
+                x = int(template_rect['x'])
+                h = int(template_rect['h'])
+                w = int(template_rect['w'])
+
+                template = frame[y:y + h, x:x + w]
+
+                kwargs['y'] = y
+                kwargs['x'] = x
+                kwargs['h'] = h
+                kwargs['w'] = w
+
                 if 'template' in save_frames:
-                    cv2.imwrite(save_frames['template'], template)
+                    # cv2.imwrite(save_frames['template'], template)
+                    center = (x + w/2, y + h/2)
+                    x, y, w, h = get_rect(center, template.shape)
+                    cv2.rectangle(color_frame, (x, y), (x + w, y + h), (0, 255, 0))
+                    cv2.circle(color_frame, center, 5, (0, 255, 0))
+                    cv2.imwrite(save_frames['template'], color_frame)
+
                 pf = pf_class(frame, template, **kwargs)
 
             # Process frame
@@ -187,9 +246,8 @@ def run_particle_filter(pf_class, video_filename, template_rect, save_frames={},
 
             # Render and save output, if indicated
             if frame_num in save_frames:
-                frame_out = frame.copy()
-                pf.render(frame_out)
-                cv2.imwrite(save_frames[frame_num], frame_out)
+                pf.render(color_frame)
+                cv2.imwrite(save_frames[frame_num], color_frame)
 
             # Update frame number
             frame_num += 1
@@ -197,11 +255,7 @@ def run_particle_filter(pf_class, video_filename, template_rect, save_frames={},
             break
 
 
-def main():
-    # Note: Comment out parts of this code as necessary
-
-    # 1a
-    # TODO: Implement ParticleFilter
+def one_a_to_d():
     run_particle_filter(ParticleFilter,  # particle filter model class
         os.path.join(input_dir, "pres_debate.avi"),  # input video
         get_template_rect(os.path.join(input_dir, "pres_debate.txt")),  # suggested template window (dict)
@@ -212,7 +266,7 @@ def main():
             84: os.path.join(output_dir, 'ps7-1-a-3.png'),
             144: os.path.join(output_dir, 'ps7-1-a-4.png')
         },  # frames to save, mapped to filenames, and 'template' if desired
-        num_particles=50)  # TODO: specify other keyword args that your model expects, e.g. measurement_noise=0.2
+        num_particles=50, sigma=10,  measurement_noise=0.2)  # TODO: specify other keyword args that your model expects, e.g. measurement_noise=0.2
 
     # 1b
     # TODO: Repeat 1a, but vary template window size and discuss trade-offs (no output images required)
@@ -224,7 +278,8 @@ def main():
     # 1d
     # TODO: Repeat 1a, but try to optimize (minimize) num_particles (no output images required)
 
-    # 1e
+
+def one_e():
     run_particle_filter(ParticleFilter,
         os.path.join(input_dir, "noisy_debate.avi"),
         get_template_rect(os.path.join(input_dir, "noisy_debate.txt")),
@@ -235,11 +290,21 @@ def main():
         },
         num_particles=50)  # TODO: Tune parameters so that model can continuing tracking through noise
 
-    # 2a
+
+def main():
+    """ Note: Comment out parts of this code as necessary"""
+
+    """ 1a """
+    one_a_to_d()
+
+    """ 1e """
+    # one_e()
+
+    """ 2a """
     # TODO: Implement AppearanceModelPF (derived from ParticleFilter)
     # TODO: Run it on pres_debate.avi to track Romney's left hand, tweak parameters to track up to frame 140
 
-    # 2b
+    """ 2b """
     # TODO: Run AppearanceModelPF on noisy_debate.avi, tweak parameters to track hand up to frame 140
 
     # EXTRA CREDIT
